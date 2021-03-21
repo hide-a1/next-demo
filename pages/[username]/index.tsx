ff43f00bab031dd8eb3b44fcd7667c91429fccd2
hide-a1 <hide210g@gmail.com>
2021-03-22 08:57:52 +0900

    feat: routing excercise

---
+++ pages/[username]/index.tsx
@@ -0,0 +1,7 @@
+export default function UserProfilePage({}) {
+  return (
+    <main>
+      <div>UserProfile</div>
+    </main>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Head from 'next/head'
+import React from 'react';
 import styles from '../styles/Home.module.css'
+import EnterPage from './enter';
+import Link from 'next/link'
+import Loader from '../components/Loader';
 
 export default function Home() {
   return (
     <div>
-
+      <Loader show/>
     </div>
   )
 }
